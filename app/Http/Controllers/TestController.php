0df9e51f5017ca3f991235081d484c268d93318b
alutfiyar <145442337+alutfiyar@users.noreply.github.com>
2023-11-16 11:44:02 +0400

    Fixes

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,26 +16,26 @@ class TestController extends Controller
         // Путь к JSON файлу теста
         $jsonFilePath = "test_data"."/".$test .".json";
         // Проверка существования файла
-        if (Storage::exists($jsonFilePath)) {
+        if (File::exists($jsonFilePath)) {
             // Чтение JSON файла
             $json = File::get($jsonFilePath);
             $testData = json_decode($json);
-    
+
             // Отправка данных теста в представление
             return view('tests.start', ['testData' => $testData]);
         } else {
             // Создание директории, если она не существует
             File::makeDirectory(dirname($jsonFilePath), $mode = 0777, true, true);
-    
+
             echo "Not found";
             echo $test;
             $testData['title'] = 'jajaj';
             //return view('tests.start', compact('testData'));
         }
-    
+
         // Обработка случая, если файл не найден
         abort(404);
     }
 
-    
+
 }
